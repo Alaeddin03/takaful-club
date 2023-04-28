1e5a56309319b95c1c626d846f755dffcd331811
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-04-28 13:31:23 +0300

    initialize project

---
+++ README.md
@@ -0,0 +1,8 @@
+## Work distribution:
+
+Khaled and Mohammed can upload HTML, CSS, and JS files into static.
+Alaa will integrate their work into react.
+
+## Required Pages:
+
+- [ ] 
---
+++ react/takaful-programs-planner/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-800 text-center'>
+        Hello there
+      </h1>
+    </>
+  )
+}
+
+export default App
