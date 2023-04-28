7d58c02108503949677da0ae8acd488e4eb5f372
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-04-28 13:45:33 +0300

    added router and root variables

--- react/takaful-programs-planner/src/routes/App.jsx
+++ react/takaful-programs-planner/src/routes/App.jsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1 className='text-blue-800 text-center'>
+      <h1 className='text-center'>
         Hello there
       </h1>
     </>
